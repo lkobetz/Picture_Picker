5dc776217fca72bdaaf3c9bba37136bb24d01430
lkobetz <larissa.kobetz@gmail.com>
2020-06-19 11:55:20 -0400

    added test for loadMore method

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -1,9 +1,32 @@
 import * as React from "react";
 import renderer from "react-test-renderer";
+import store from "../../store/index";
 
 import { MonoText } from "../StyledText";
 import { callApi } from "../../api/call";
-import * as axios from "axios";
+import AllImages from "../AllImages";
+import { JSDOM } from "jsdom";
+import { configure, mount, render, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import "react-native-mock-render/mock";
+
+configure({ adapter: new Adapter() });
+
+const { window } = new JSDOM();
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: "node.js",
+};
+copyProps(window, global);
 
 it(`renders correctly`, () => {
   const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
@@ -12,8 +35,16 @@ it(`renders correctly`, () => {
 });
 
 describe("callApi", () => {
-  it("successfully fetches data from an API", async () => {
-    const result = await callApi("cats", 1, 3);
-    expect(result.hits.length).toEqual(3);
+  it("returns all results in an array", async () => {
+    const result = await callApi("cats", 50, 10);
+    expect(result.hits.length).toEqual(10);
+  });
+});
+
+describe("loadMore", () => {
+  it("is a function", () => {
+    expect(typeof AllImages.WrappedComponent.prototype.loadMore).toBe(
+      "function"
+    );
   });
 });
